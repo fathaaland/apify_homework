5aed8eed3a35732ceb4dfe4321ba6672ec0385b5
David Dobrovolny <dejv.dobrovolny642001@gmail.com>
2026-02-14 13:07:46 +0100

    create fake api call

--- homework.js
+++ homework.js
@@ -1,7 +1,12 @@
 //main array for saving finding products
 const allProducts = [];
 
+let minPrice;
+let maxPrice;
 
-//
-const minPrice;
-const maxPrice;
+async function getProducts(minPrice, maxPrice) {
+  //url creation
+  let url = `https://api.ecommerce.com/products/?minPrice=${minPrice}&&/maxPrice=${maxPrice}`;
+  //no-specific API call
+  let data = await getApiCall(minPrice, maxPrice);
+}

f7859a288eb8af3dadd2fd6e1da7c0008c3bbad5
David Dobrovolny <dejv.dobrovolny642001@gmail.com>
2026-02-20 14:55:51 +0100

    homework number 3 is completed test

--- test/homework_test_3.js
+++ test/homework_test_3.js
@@ -1,32 +1,56 @@
-// Úkol: "Vyzobej názvy z HTML"
-// Představ si, že proměnná $ je tvoje "kouzelná hůlka" (v Node.js je to knihovna Cheerio), která umí prohledávat HTML.
+{
+  /* <div class="item">
+  <a href="/p/iphone-15" class="name-link">
+     iPhone 15 (Skladem) 
+  </a>
+  <span class="price-tag">Cena: 25 000 CZK</span>
+</div>
+<div class="item">
+  <a href="/p/samsung-s24" class="name-link">
+     Samsung S24 (Poslední kus) 
+  </a>
+  <span class="price-tag">Cena: 21 990 CZK</span>
+</div>
 
-// Máš tuhle strukturu v HTML:
+Tvůj cíl: Vytvoř pole objektů, kde:
 
-// HTML
-// <ul id="product-list">
-//   <li class="item">Produkt A</li>
-//   <li class="item">Produkt B</li>
-//   <li class="item">Produkt C</li>
-// </ul>
-// Tvůj cíl: Dostaň názvy produktů do jednoho pole: ['Produkt A', 'Produkt B', 'Produkt C'].
+name: Bude jen název produktu (bez toho "(Skladem)" atd.). Nápověda: použij .split('(')[0].trim().
 
-function extractNames($) {
+price: Bude čisté číslo (např. 25000). Nápověda: použij .replace(...) nebo regulární výraz a pak Number().
+
+url: Bude celá adresa. Všimni si, že v HTML je jen /p/iphone-15. Musíš k tomu připojit https://www.obchod.cz.
+
+Nápověda: Pro získání odkazu použij $(element).find('a').attr('href'). */
+}
+
+function getRequiredData(data) {
   const results = [];
-  let getAllElementsWithItem = $(".item");
+  const getAllElements = data(".item");
 
-  getAllElementsWithItem.each((index, element) => {
-    const required_text = $(element).text();
-    results.push(required_text);
+  getAllElements.each((index, element) => {
+    let element_name = $(element).find(".name-link").split("(")[0].trim();
+    let element_price = $(element).find(".price-tag").parseInt();
+
+    const obj = { name: element_name, price: element_price };
+    results.push(obj);
   });
 
   return results;
 }
 
-extractNames(
-  <ul id="product-list">
-    <li class="item">Produkt A</li>
-    <li class="item">Produkt B</li>
-    <li class="item">Produkt C</li>
-  </ul>
+getRequiredData(
+  <>
+    <div class="item">
+      <a href="/p/iphone-15" class="name-link">
+        iPhone 15 (Skladem)
+      </a>
+      <span class="price-tag">Cena: 25 000 CZK</span>
+    </div>
+    <div class="item">
+      <a href="/p/samsung-s24" class="name-link">
+        Samsung S24 (Poslední kus)
+      </a>
+      <span class="price-tag">Cena: 21 990 CZK</span>
+    </div>
+  </>
 );
